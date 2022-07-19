aaefb9360fbc2af30bfcc9022cf8c5f4aa3a444c
John Kibirige <kibirigejohn005@gmail.com>
2022-07-19 15:23:27 +0300

    remove overflow hidden property on the html when links and chevron images are clicked

--- js/index.js
+++ js/index.js
@@ -3,11 +3,47 @@ const humberger = document.querySelector('.humberger');
 const headerLinks = document.querySelector('.header-links');
 const headerLogo = document.querySelector('.animate-logo');
 const blur = document.querySelector('.blur-effect');
+const htmlElement = document.documentElement;
 
+// chevron images
+const chevronImages = document.querySelectorAll('.nav-menu li img');
+const navMenuLinks = document.querySelectorAll('.nav-menu li a');
+
+// add event on humberger menu
 humberger.addEventListener('click', () => {
+  toggleActions();
+});
+
+// add event listener on the chevron images in the nav-menu
+chevronImages.forEach((chevron) => {
+  chevron.addEventListener('click', () => {
+    window.location.href = `${
+      chevron.id === 'forAbout'
+        ? '#about'
+        : chevron.id === 'forContact'
+        ? '#contact'
+        : chevron.id === 'forPortfolio'
+        ? '#portfolio'
+        : ''
+    }`;
+
+    // remove overflow hidden on the html by toggling the active class
+
+    toggleActions();
+  });
+});
+
+// add event listener on the anchor tags inside nav menu
+navMenuLinks.forEach((anchor) => {
+  anchor.addEventListener('click', () => {
+    toggleActions();
+  });
+});
+
+function toggleActions() {
+  htmlElement.classList.toggle('active');
   humberger.classList.toggle('active');
   headerLinks.classList.toggle('active');
   headerLogo.classList.toggle('active');
   blur.classList.toggle('active');
-  document.documentElement.classList.toggle('active');
-});
+}
